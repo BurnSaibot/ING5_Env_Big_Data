76a5cc8b48b999ce3e2248ec0ca141b9eed7ba2c
Eric Cerveau <eric.cerveau@edu.ece.fr>
2021-12-26 14:39:01 +0100

    First Draft authentication through krb

--- twitterKafkaProducer/src/main/java/fr/edu/ece/Main.java
+++ twitterKafkaProducer/src/main/java/fr/edu/ece/Main.java
@@ -1,6 +1,7 @@
 package fr.edu.ece;
 
 import fr.edu.ece.twitter_consumer.TwitterConsumer;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
@@ -36,12 +37,18 @@ public class Main {
             LOGGER.info("Parsing configuration file...");
             props.load(input);
 
+            System.setProperty("java.security.krb5.conf", props.getProperty("kerberos.configuration.path"));
+            System.setProperty("java.security.auth.login.config", props.getProperty("kerberos.configuration.jaas.path"));
+            System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+            System.setProperty("sun.security.krb5.debug", "true");
+
             queue = new LinkedBlockingQueue<>();
             Properties kafkaProps = new Properties();
             kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, props.getProperty("kafka.producer.bootstrap-server"));
             kafkaProps.put(ProducerConfig.CLIENT_ID_CONFIG, "twitterKafkaProducer");
             kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
             kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            kafkaProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, props.getProperty("kerberos.configuration.protocol"));
 
             LOGGER.info("Creating twitter client...");
             tc = new TwitterConsumer(
